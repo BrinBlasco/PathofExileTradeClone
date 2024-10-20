47e3f0ed60482fbf23a7ab2f7346493d3e422dba
BrinBlasco <62804590+BrinBlasco@users.noreply.github.com>
2024-10-20 22:29:07 +0200

    added more items...
    to the items.json, more dropdown functionality

--- script.js
+++ script.js
@@ -29,46 +29,43 @@ filterGroups.forEach((group) => {
 
 function load(elId) {
   const dropdown = document.querySelector(`#${elId} ul`);
-  console.log(dropdown);
-  print(dropdown);
   const item_category = JSON.parse(
     localStorage.getItem("type_filters")
   ).item_category;
-  console.log(item_category);
 
-  // Loop through item_category to create new list items
   for (let el of item_category) {
-    const newListItem = document.createElement("li");
-    const newSpan = document.createElement("span");
-
-    newSpan.className = "list-item";
-    newSpan.textContent = el;
-
-    newListItem.appendChild(newSpan);
-    dropdown.appendChild(newListItem);
+    dropdown.insertAdjacentHTML(
+      "beforeend",
+      `<li><span class="list-item">${el}</span></li>`
+    );
   }
 }
 
 async function getItems() {
-  if (localStorage.length != 0) {
-    fetch("/items.json")
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error("Network response was not ok " + response.statusText);
+  let outsideData = null;
+  await fetch("/items.json")
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok " + response.statusText);
+      }
+      return response.json();
+    })
+    .then((data) => {
+      for (const section in data) {
+        if (data.hasOwnProperty(section)) {
+          localStorage.setItem(section, JSON.stringify(data[section]));
         }
-        return response.json();
-      })
-      .then((data) => {
-        for (const section in data) {
-          if (data.hasOwnProperty(section)) {
-            localStorage.setItem(section, JSON.stringify(data[section]));
-          }
-        }
-        console.log(data);
-        load("item_category");
-      })
-      .catch((error) => console.error("Error fetching items:", error));
-  }
+      }
+      outsideData = data;
+    })
+    .catch((error) => console.error("Error fetching items:", error));
+
+  load("item_category");
+}
+
+function toggleFilters() {
+  const filters = document.getElementsByClassName("filters")[0];
+  filters.style.display = filters.style.display == "none" ? "block" : "none";
 }
 
 getItems();
