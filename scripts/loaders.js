f99fd7e49a9ac930619c233119bf4121e4b8eceb
BrinBlasco <62804590+BrinBlasco@users.noreply.github.com>
2024-10-23 22:07:52 +0200

    kuhamo

--- scripts/loaders.js
+++ scripts/loaders.js
@@ -1,33 +1,19 @@
-function load(elId) {
-  const dropdown = document.querySelector(`#${elId} ul`);
-  const item_category = JSON.parse(
-    localStorage.getItem(elId)
-  );
-
-  for (let el of item_category) {
-    dropdown.insertAdjacentHTML(
-      "beforeend",
-      `<li><span class="list-item" id=${el}>${el}</span></li>`
-    );
-  }
-}
 
 function load_section(section_id) {
   const section_data = JSON.parse(localStorage.getItem(section_id));
-  console.log(section_data);
 
   for(let section in section_data){
     const section_dropdown = document.querySelector(`#${section_id} #${section}.dropdown ul`);
-    const section_data = JSON.parse(localStorage.getItem(section));
+    const section_items = section_data[section];
+    console.log(section_items);
 
-    for (let element of section_data) {
+    for (let element of section_items) {
       section_dropdown.insertAdjacentHTML(
         "beforeend",
         `<li><span class="list-item" id=${element}>${element}</span></li>`
       );
     }
   }
-
 }
 
 
@@ -52,11 +38,13 @@ async function getItems() {
   // for (let section of outsideData.sections) {
   //   load_section(section);
   // }
+
   load_section("type_filters");
-  load_section("character_class");
-  load_section("type_filters");
-  load_section("blighted_map");
-  load_section("blight_ravaged_map");
+  load_section("requirements");
+  load_section("map_filters");
+  load_section("heist_filters");
+  load_section("ultimatum_filters");
+  load_section("miscellaneous_filters");
   load_section("map_completion_reward");
 }
 
